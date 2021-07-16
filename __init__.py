a86d9a3286b5d79f83e8747a6f8c5f16813bfad6
Jeffrey Aven (DPC) <jeffrey.aven@dpc.vic.gov.au>
2021-07-16 19:47:28 +1000

    added dbfilepath argument

--- __init__.py
+++ __init__.py
@@ -8,7 +8,12 @@ class InfraQL:
 			defaultbin = r'infraql.exe'
 		self.exe = kwargs.get('exe', defaultbin)
 		self.keyfilepath = kwargs.get('keyfilepath')
+		self.dbfilepath = kwargs.get('dbfilepath')
 		self.params = ['exec', '--keyfilepath', self.keyfilepath, '--output', 'json']
+		if self.dbfilepath is not None:
+			self.params.append('--dbfilepath')
+			self.params.append(self.dbfilepath)
+			
 	def execute(self, query):
 		local_params = self.params
 		local_params.insert(1, query)
@@ -16,6 +21,7 @@ class InfraQL:
 			iqlPopen = subprocess.Popen([self.exe] + local_params,
                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 			output = iqlPopen.stdout.read()
+			iqlPopen.terminate()
 		except:
 			e = sys.exc_info()[0]
 			print("ERROR %s %s" % (str(e), e.__doc__))
